28624901973c0ac0e1ae48ae5aae4e84275aae2a
invaavni <invaavni1994@hotmail.com>
2016-02-02 18:19:45 +0100

    Shoot fixed

--- Assets/Scripts/ShootPlayer.js
+++ Assets/Scripts/ShootPlayer.js
@@ -7,7 +7,7 @@ private var animator : Animator;
 
 
 function Start () {
-	animator = transform.GetChild(1).gameObject.GetComponent.<Animator>();
+	animator = transform.parent.gameObject.transform.GetChild(2).gameObject.GetComponent.<Animator>();
 }
 
 function Update () {
@@ -17,43 +17,50 @@ function Update () {
 			var bullet0 : Rigidbody2D = Rigidbody2D.Instantiate(projectile,
 										transform.position,transform.rotation);
 			bullet0.velocity = transform.right*bulletSpeed;
-			Physics2D.IgnoreCollision(bullet0.GetComponent.<Collider2D>(), GetComponent.<Collider2D>());
+			Physics2D.IgnoreCollision(bullet0.GetComponent.<Collider2D>(), transform.parent.gameObject.GetComponent.<Collider2D>());
+			Physics2D.IgnoreCollision(bullet0.GetComponent.<Collider2D>(), transform.parent.gameObject.GetComponent.<CircleCollider2D>());
 		}
 		else if(animator.GetFloat("lookingAt") == 1.0f) {
 			var bullet1 : Rigidbody2D = Rigidbody2D.Instantiate(projectile,
 										transform.position,transform.rotation);
 			bullet1.velocity = -transform.right*bulletSpeed;
-			Physics2D.IgnoreCollision(bullet1.GetComponent.<Collider2D>(), GetComponent.<Collider2D>());
+			Physics2D.IgnoreCollision(bullet1.GetComponent.<Collider2D>(), transform.parent.gameObject.GetComponent.<Collider2D>());
+			Physics2D.IgnoreCollision(bullet1.GetComponent.<Collider2D>(), transform.parent.gameObject.GetComponent.<CircleCollider2D>());
 		}
 		else if(animator.GetFloat("lookingAt") == 0.75f || animator.GetFloat("lookingAt") == 1.75f) {
 			var bullet2 : Rigidbody2D = Rigidbody2D.Instantiate(projectile,
 										transform.position,transform.rotation);
 			bullet2.velocity = transform.up*bulletSpeed;
-			Physics2D.IgnoreCollision(bullet2.GetComponent.<Collider2D>(), GetComponent.<Collider2D>());
+			Physics2D.IgnoreCollision(bullet2.GetComponent.<Collider2D>(), transform.parent.gameObject.GetComponent.<Collider2D>());
+			Physics2D.IgnoreCollision(bullet2.GetComponent.<Collider2D>(), transform.parent.gameObject.GetComponent.<CircleCollider2D>());
 		}
 		else if(animator.GetFloat("lookingAt") == 1.5f) {
 			var bullet3 : Rigidbody2D = Rigidbody2D.Instantiate(projectile,
 										transform.position,transform.rotation);
 			bullet3.velocity = (transform.up+transform.right)*bulletSpeed;
-			Physics2D.IgnoreCollision(bullet3.GetComponent.<Collider2D>(), GetComponent.<Collider2D>());
+			Physics2D.IgnoreCollision(bullet3.GetComponent.<Collider2D>(), transform.parent.gameObject.GetComponent.<Collider2D>());
+			Physics2D.IgnoreCollision(bullet3.GetComponent.<Collider2D>(), transform.parent.gameObject.GetComponent.<CircleCollider2D>());
 		}
 		else if(animator.GetFloat("lookingAt") == 0.5f) {
 			var bullet4 : Rigidbody2D = Rigidbody2D.Instantiate(projectile,
 										transform.position,transform.rotation);
 			bullet4.velocity = (transform.up-transform.right)*bulletSpeed;
-			Physics2D.IgnoreCollision(bullet4.GetComponent.<Collider2D>(), GetComponent.<Collider2D>());
+			Physics2D.IgnoreCollision(bullet4.GetComponent.<Collider2D>(), transform.parent.gameObject.GetComponent.<Collider2D>());
+			Physics2D.IgnoreCollision(bullet4.GetComponent.<Collider2D>(), transform.parent.gameObject.GetComponent.<CircleCollider2D>());
 		}
 		else if(animator.GetFloat("lookingAt") == 0.25f) {
 			var bullet5 : Rigidbody2D = Rigidbody2D.Instantiate(projectile,
 										transform.position,transform.rotation);
 			bullet5.velocity = -(transform.up+transform.right)*bulletSpeed;
-			Physics2D.IgnoreCollision(bullet5.GetComponent.<Collider2D>(), GetComponent.<Collider2D>());
+			Physics2D.IgnoreCollision(bullet5.GetComponent.<Collider2D>(), transform.parent.gameObject.GetComponent.<Collider2D>());
+			Physics2D.IgnoreCollision(bullet5.GetComponent.<Collider2D>(), transform.parent.gameObject.GetComponent.<CircleCollider2D>());
 		}
 		else if(animator.GetFloat("lookingAt") == 1.25f) {
 			var bullet6 : Rigidbody2D = Rigidbody2D.Instantiate(projectile,
 										transform.position,transform.rotation);
 			bullet6.velocity = (-transform.up+transform.right)*bulletSpeed;
-			Physics2D.IgnoreCollision(bullet6.GetComponent.<Collider2D>(), GetComponent.<Collider2D>());
+			Physics2D.IgnoreCollision(bullet6.GetComponent.<Collider2D>(), transform.parent.gameObject.GetComponent.<Collider2D>());
+			Physics2D.IgnoreCollision(bullet6.GetComponent.<Collider2D>(), transform.parent.gameObject.GetComponent.<CircleCollider2D>());
 		}
 	}
 
