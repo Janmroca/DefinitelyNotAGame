9315c53d3b22e02ddee4ba97881f7d8379c4780f
jan <Jmanrique94@hotmail.com>
2016-02-03 18:08:32 +0100

    Fixing bullet despawn samus

--- Assets/Scripts/SamusBulletDespawn.js
+++ Assets/Scripts/SamusBulletDespawn.js
@@ -1,7 +1,7 @@
 ﻿#pragma strict
 
 public var lifeTime : float = 0.0f;
-public var Damage : int = 0;
+public var explosion : GameObject;
 
 private var tiempo : float;
 
@@ -13,16 +13,11 @@ function Update () {
 	tiempo += Time.deltaTime;
 	if(tiempo > lifeTime) {
 		Destroy(gameObject);
+		Instantiate(explosion, transform.position, transform.rotation);
 	}
 }
 
 function OnCollisionEnter2D (col : Collision2D) {
 	Instantiate(explosion, transform.position, transform.rotation);
-	if(col.collider.gameObject.tag == "Player")
-		col.collider.gameObject.SendMessage("decreaseLife",Damage);
-	else if(col.collider.gameObject.tag == "Stormtropper")
-		col.collider.gameObject.transform.GetChild(0).SendMessage("decreaseLife");
-	else if(col.collider.gameObject.tag == "Creeper")
-		col.collider.gameObject.SendMessage("decreaseLife");
 	Destroy(gameObject);
 }
