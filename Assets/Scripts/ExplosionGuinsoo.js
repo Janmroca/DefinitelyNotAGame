17723badaf3acff1803c0441c271a824ec7c98fc
invaavni <invaavni1994@hotmail.com>
2016-02-03 21:31:25 +0100

    Guinsoo

---
+++ Assets/Scripts/ExplosionGuinsoo.js
@@ -0,0 +1,18 @@
+﻿#pragma strict
+
+public var speed : float = 15;
+public var maxRadius : float = 5;
+
+private var collision : boolean = false;
+
+function Update () {
+	transform.localScale += Vector3(1,1,1)*speed*Time.deltaTime;
+	if(transform.localScale.x >= maxRadius) Destroy(gameObject);
+}
+
+function OnTriggerEnter2D(col : Collider2D) {
+	if(col.GetComponent.<Collider2D>().gameObject.tag == "Stormtropper")
+		col.GetComponent.<Collider2D>().gameObject.transform.GetChild(0).SendMessage("decreaseLife");
+	else if(col.GetComponent.<Collider2D>().gameObject.tag == "Creeper")
+		col.GetComponent.<Collider2D>().gameObject.SendMessage("decreaseLife");
+}
