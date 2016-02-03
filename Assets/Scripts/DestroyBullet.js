7d9c17258dd50b60d3e705ddde241658de57e99d
invaavni <invaavni1994@hotmail.com>
2016-02-03 17:34:02 +0100

    arreglando prefabs

--- Assets/Scripts/DestroyBullet.js
+++ Assets/Scripts/DestroyBullet.js
@@ -1,11 +1,8 @@
 ﻿#pragma strict
 
 public var lifeTime : float = 0.0f;
-<<<<<<< HEAD
 public var Damage : int = 0;
-=======
 public var explosion : GameObject;
->>>>>>> origin/master
 
 private var tiempo : float;
 
@@ -22,15 +19,11 @@ function Update () {
 }
 
 function OnCollisionEnter2D (col : Collision2D) {
-<<<<<<< HEAD
 	if(col.collider.gameObject.tag == "Player")
 		col.collider.gameObject.SendMessage("decreaseLife",Damage);
 	else if(col.collider.gameObject.tag == "Stormtropper")
 		col.collider.gameObject.transform.GetChild(0).SendMessage("decreaseLife");
 	else if(col.collider.gameObject.tag == "Creeper")
 		col.collider.gameObject.SendMessage("decreaseLife");
-=======
-	Instantiate(explosion, transform.position, transform.rotation);
->>>>>>> origin/master
 	Destroy(gameObject);
 }
