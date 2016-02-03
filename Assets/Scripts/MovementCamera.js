324635b314fb4c45b17952f3f4e903ea574816a3
jan <Jmanrique94@hotmail.com>
2016-02-03 21:32:08 +0100

    fixcam

--- Assets/Scripts/MovementCamera.js
+++ Assets/Scripts/MovementCamera.js
@@ -1,23 +1,15 @@
 ﻿#pragma strict
 
 public var player : GameObject;
+public var minX : float;
+public var maxX : float;
 
-private var freeze : boolean;
 
 function Start() {
-	freeze = false;
 }
 
 function Update () {
 	var playerPos : Vector2 = player.transform.position;
 	var camPos : Vector3 = transform.position;
-	if (!freeze) transform.position = Vector3(playerPos.x, camPos.y, camPos.z);
-}
-
-function OnCollisionEnter(col : Collision) {
-	freeze = true;
-}
-
-function OnCollisionExit(col : Collision) {
-	freeze = false;
+	if (playerPos.x > minX && playerPos.x < maxX) transform.position = Vector3(playerPos.x, camPos.y, camPos.z);
 }
