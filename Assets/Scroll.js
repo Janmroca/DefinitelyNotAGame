35e06b127cea73351e5b21d8cec35f9a79207936
jan <Jmanrique94@hotmail.com>
2016-02-03 20:09:29 +0100

    Level0

---
+++ Assets/Scroll.js
@@ -0,0 +1,13 @@
+﻿#pragma strict
+
+public var speed : float = 0.5f;
+
+function Start () {
+
+}
+
+function Update () {
+	var offset : Vector2 = Vector2(Time.time * speed, 0);
+
+	GetComponent.<Renderer>().material.mainTextureOffset = offset;
+}
