5fe58ff666c4e4273fee6f8bf6d40bb19307c26c
invaavni <invaavni1994@hotmail.com>
2016-02-03 22:30:08 +0100

    Guinsoo fixed

---
+++ Assets/Scripts/ConfigGuinsoo.js
@@ -0,0 +1,12 @@
+﻿#pragma strict
+
+public var explosion : Rigidbody2D;
+
+private var activated : boolean = false;
+
+function OnTriggerEnter2D(col : Collider2D) {
+	if(col.GetComponent.<Collider2D>().gameObject.tag == "Player") {
+		col.GetComponent.<Collider2D>().gameObject.transform.GetChild(0).GetComponent.<ShootPlayer>().projectile = explosion;
+		Destroy(gameObject);
+	}
+}
