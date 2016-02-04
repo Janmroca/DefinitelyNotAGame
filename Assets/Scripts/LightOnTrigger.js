be264bed6820a57f021c1b033dd7925a342ca6b6
jan <Jmanrique94@hotmail.com>
2016-02-04 17:35:50 +0100

    Finished castlevania level

--- Assets/Scripts/LightOnTrigger.js
+++ Assets/Scripts/LightOnTrigger.js
@@ -3,16 +3,22 @@
 public var background : GameObject;
 
 private var triggered : boolean = false;
+private var time : float = 0.0f;
+
+function Start() {
+	time = 0.0f;
+}
+
+function Update() {
+	time += Time.deltaTime;
+	if (time > 18) {
+		triggered = true;
+		GetComponent.<ZombieSpawn>().enabled = true;
+	}
+}
 
 function FixedUpdate() {
 	if (triggered) {
 		background.GetComponent.<SpriteRenderer>().color += Vector4(0.01,0.01,0.01,0);
-	}
-}
-
-function OnTriggerEnter2D(col: Collider2D) {
-	if (col.gameObject.tag == "Player") {
-		triggered = true;
-		GetComponent.<ZombieSpawn>().enabled = true;
 	}
 }
--- Assets/Scripts/ZombieSpawn.js
+++ Assets/Scripts/ZombieSpawn.js
@@ -6,19 +6,16 @@ public var zombie : GameObject;
 private var triggered : boolean = false;
 private var time : float = 0.0f;
 
+function Start() {
+	time = 5.0f;
+}
+
 function Update () {
-	time += Time.deltaTime;
-	if (triggered && time > timeToSpawn) {
+	if (time > timeToSpawn) {
 		time = 0;
 		var xoffset : float = Random.Range(-39, 39);
 		Instantiate(zombie, Vector3(xoffset, -4.1, 0), Quaternion());
+		Instantiate(zombie, Vector3(-xoffset, -4.1, 0), Quaternion());		
 	}
 
-}
-
-function OnTriggerEnter2D(col: Collider2D) {
-	if (col.gameObject.tag == "Player") {
-		triggered = true;
-		time = 5.0f;
-	}
 }
